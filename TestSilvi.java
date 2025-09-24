c3ff484f00a5f8bfce5304bb8ecfbf9f3f174156
artasdaa <155645958+artasdaa@users.noreply.github.com>
2025-09-24 11:38:31 +0700

    Update TestSilvi.java

--- TestSilvi.java
+++ TestSilvi.java
@@ -1,6 +1,6 @@
 public class TestSilvi {
     public static void main(String[] args) {
         System.out.println("Hi, ini bsris kode pertama yang dimodifikasi user 2");
-        System.out.println("User 2 menambahkan baris baru");
+        System.out.println("User 2 menambahkan baris baru");}
     
 }
